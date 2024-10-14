61cbb8e2674db9cd8594c08ff61699a2da8adbef
hairus <zhomenep@gmail.com>
2024-10-14 11:39:31 +0700

    fix export

--- app/Exports/UhExport.php
+++ app/Exports/UhExport.php
@@ -22,9 +22,9 @@ class UhExport implements FromView
 
     public function view(): View
     {
-        $siswa = siswas::where('rombel', 'X-A')->orderBy('name', 'ASC')->get();
-        $mapel = mapels::where('id', $this->mapel_id)->first();
         $kelas = kelas::where('id', $this->kelas_id)->first();
+        $siswa = siswas::where('rombel', $kelas->kelas)->orderBy('name', 'ASC')->get();
+        $mapel = mapels::where('id', $this->mapel_id)->first();
 
         return view('guru.exportUH', [
             'siswas' => $siswa,
